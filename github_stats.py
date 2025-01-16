d4c00cae31485a5ac7c4bc81b257bd13e475d96d
Yiru Jiao <58258668+Yiru-Jiao@users.noreply.github.com>
2025-01-16 21:12:34 +0100

    Update github_stats.py

--- github_stats.py
+++ github_stats.py
@@ -537,7 +537,7 @@ async def main() -> None:
             "ACCESS_TOKEN and GITHUB_ACTOR environment variables cannot be None!"
         )
     async with aiohttp.ClientSession() as session:
-        s = Stats(user, access_token, session, exclude_langs={"Jupyter Notebook"})
+        s = Stats(user, access_token, session)
         print(await s.to_str())
 
 
